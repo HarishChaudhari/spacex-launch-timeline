4480321f719bc2032a9659d7e1bf441be611776c
Harish <harish.chaudhari16@gmail.com>
2022-11-30 16:50:44 +0530

    some restructruing, added few more controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,107 @@
 "use client"
 import Image from 'next/image'
 import styles from './page.module.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { time } from 'console';
+
+var timerInterval: any;
+var rotationInterval: any;
 
 export default function Home() {
-    
-    useEffect(() => {
-        // You now have access to `window`
-        console.log('aaa');
-        
-        function elementsOverlap(el1, el2) {
-            const domRect1 = el1.getBoundingClientRect();
-            const domRect2 = el2.getBoundingClientRect();
-            const min = -5;
+    const [timeValue, setTimeValue] = useState('0');
+    const [timerClock, setTimerClock] = useState('T - 00:00:00');
+    const [is_started, setIsStarted] = useState(false);
+    const [rotationAngle, setRotationAngle] = useState(0);
 
-            console.log(domRect1.left - min);
-            console.log(domRect2.right);
-            
-            // return !(
-            //     domRect1.top - min > domRect2.bottom ||
-            //     domRect1.right - min < domRect2.left ||
-            //     domRect1.bottom - min < domRect2.top ||
-            //     domRect1.left - min > domRect2.right
-            // );
-            return !(domRect1.left - min > domRect2.right);
+    function startLaunch(){
+        if( is_started === true ) {
+            setIsStarted(false);
+            clearInterval(timerInterval);
+            clearInterval(rotationInterval);
+            console.log("a");
+        } else {
+            setIsStarted(true);
+            setTimer(timeValue);
+            startRotation();
         }
-        
-        setInterval(function () {
-            console.log('===============================================');
+    }
+    function setAngle(e) {
+        setRotationAngle(e.target.value);
+    }
 
+    function updateTimer(e) {
+        clearInterval(timerInterval);
+        clearInterval(rotationInterval);
+        
+        return setTimeValue(e.target.value);
+    }
+
+    function setTimer ( input : any ) {
+        clearInterval(timerInterval);
+        let duration = input * 60;
+        let timer = duration, hours, minutes, seconds;
+        timerInterval = setInterval(function () {
+            hours = Math.floor(timer / 3600);
+            minutes = Math.floor(timer % 3600 / 60);
+            seconds = Math.floor(timer % 3600 % 60);
+            
+            hours = hours < 10 ? "0" + hours : hours;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            
+            setTimerClock("T - " + hours + ":" + minutes + ":" + seconds);
+
+            if (--timer < 0) {
+                // clear interval as we don't want to repeat the countdown
+                clearInterval(timerInterval);
+            }
+        }, 1000);
+    };
+    
+    function elementsOverlap(el1 : any, el2 : any) {
+        const domRect1 = el1.getBoundingClientRect();
+        const domRect2 = el2.getBoundingClientRect();
+        const min = -5; // offset
+
+        // this is to find the exact overlap
+        // return !(
+        //     domRect1.top - min > domRect2.bottom ||
+        //     domRect1.right - min < domRect2.left ||
+        //     domRect1.bottom - min < domRect2.top ||
+        //     domRect1.left - min > domRect2.right
+        // );
+        return !(domRect1.left - min > domRect2.right);
+    }
+    function elementOutside(el1 : any, el2 : any) {
+        const domRect1 = el1.getBoundingClientRect();
+        const domRect2 = el2.getBoundingClientRect();
+        const min = -5; // offset
+        return (domRect1.top - min > domRect2.bottom + 200);
+    }
+    
+    function startRotation() {
+        clearInterval(rotationInterval);
+        rotationInterval = setInterval(function () {
             //rotate the circle
             var angle = parseFloat( jQuery('#page_circle_wrapper__RmoWD').attr('data-angle') );
-            angle = angle - 0.09;
-            // console.log(angle);
+            angle = angle - rotationAngle;
+
             jQuery('#page_circle_wrapper__RmoWD').css('transform', 'rotate('+ angle +'deg)');
             jQuery('#page_circle_wrapper__RmoWD').attr('data-angle', angle);
 
-            var threshold = jQuery('.page_timeline_wrapper__ybJhT').offset().top;
-            var bottomThreshold = threshold + jQuery('.page_timeline_wrapper__ybJhT').outerHeight();
-            // console.log(threshold);
-            
-            jQuery('#nodes li').each(function (threshold, bottomThreshold) {
+            jQuery('#nodes li').each(function () {
                 
                 // var matrix = jQuery(this).css('transform').replace(/[^0-9\-.,]/g, '').split(',');
                 // var x = matrix[12] || matrix[4];
                 var elementTop = jQuery(this).offset().top;
-                //console.log( elementTop );
+                
                 if (elementsOverlap(jQuery(this).find('div')[0], jQuery('#page_marker__Y9t1D')[0]) === true ) {
                     jQuery(this).addClass('page_done__0RB5C');
                 }
                 // check if element goes outside div remove done class
+                if (elementOutside(jQuery(this).find('div')[0], jQuery('.page_timeline_wrapper__ybJhT')[0]) === true) {
+                    jQuery(this).removeClass('page_done__0RB5C');
+                }
                 
             });
 
@@ -80,7 +132,12 @@ export default function Home() {
             });
             */
         }, 100);
-        
+    }
+
+    useEffect(() => {
+
+        // You now have access to `window`
+
     }, []);
 
     return (
@@ -96,22 +153,37 @@ export default function Home() {
                 </p> */}
 
                 <div className={styles.grid}>
-                    <a href="#" className={styles.card}>
-                        <h2>Add Event Node &rarr;</h2>
-                        <p>Add event node in timeline</p>
+                    <a className={styles.card}>
+                        <h2>Start/Stop</h2>
+                        <p>
+                            <button className={styles.button} onClick={startLaunch}>
+                                {is_started === false ? "Start" : "Stop"}   
+                            </button>
+                        </p>
                     </a>
 
-                    <a href="#" className={styles.card}>
-                        <h2>Set Lanuch Time &rarr;</h2>
-                        <p>For T- and T+ will be calculation.</p>
+                    <a className={styles.card}>
+                        <h2>Time to Lanuch &rarr;</h2>
+                        <p>
+                            <input className={styles.input_text}
+                                value={timeValue}
+                                onChange={updateTimer}
+                            />
+                            In Minutes.</p>
                     </a>
 
-                    <a href="#" className={styles.card}>
-                        <h2>Timeline Zoom &rarr;</h2>
-                        <p>Zoom in or Zoom Out the timeline.</p>
+                    <a className={styles.card}>
+                        <h2>Rotation Angle &rarr;</h2>
+                        <p>
+                            <input className={styles.input_text}
+                                value={rotationAngle}
+                                onChange={setAngle}
+                            />
+                            In degrees.
+                        </p>
                     </a>
 
-                    <a href="#" className={styles.card}>
+                    <a className={styles.card}>
                         <h2>Altitude Interval &rarr;</h2>
                         <p>Rate for Altitude calculation /sec.</p>
                     </a>
@@ -132,6 +204,8 @@ export default function Home() {
                             <li data-current-x="180" data-current-y="412"><div className={styles.node_circle}></div><span>Deploy</span></li>
                         </ul>
                     </div>
+
+                    <div className={styles.timer_clock}>{timerClock}</div>
                 </div>
             </main>
         </div>
