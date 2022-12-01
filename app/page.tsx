4ba365e48a5c05f81b7725f52f233f1316ea7717
Harish <harish.chaudhari16@gmail.com>
2022-12-01 18:19:50 +0530

    added github button, some text changes

--- app/page.tsx
+++ app/page.tsx
@@ -168,8 +168,7 @@ export default function Home() {
                     <p className={styles.fun_desc}>I could have made it a lot better, but I think this is good enough to play with. :) 
                         <br />Besides, I was just experimenting with <a href="https://nextjs.org/blog/next-13" target="_blank" rel="noreferrer">Next.js 13</a> and wanted a cool idea with smaller interactions.
                         <br/>This is desktop only, did not have enough time to make it work on different screens.
-                        If an algorithm is devised for plotting the nodes based on the actual mission time then it can become responive and one can create whole lot of other features such as zoom in/out the timeline, add more interations.
-                        If I get some time in future then I will try to create one.
+                        If an algorithm is devised for plotting the nodes based on the actual mission time, then a whole lot of other features such as zoom in/out the timeline, add more interations.
                     </p>
                 </div>
             </main>
